20783db04f7b10b4b71228bea9b07cac45fe7ea6
manasaa15 <132209899+manasaa15@users.noreply.github.com>
2023-10-25 18:16:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputbox = document.getElementById('inputbox');
+const listcontainer = document.getElementById('list')
+
+function addtask(){
+    if(inputbox.value === ''){
+        window.alert("you must write something!");
+
+    }
+    else{
+        let li = document.createElement('li')
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+        let span =document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span)
+    }
+    inputbox.value="";
+    savedata();
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false);
+
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+
+
+}
+
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+
+showtask();
